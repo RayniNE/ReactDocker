d184bd1eef06a52fdfeb0cdd0c3b0eefd2379d24
Rayni Nunez Espino <timetopwm@gmail.com>
2022-12-29 15:44:58 -0400

    Modified the Dockerfile.dev so it uses node 18, modified the docker-compose.yaml and specified the platform to be linux/arm64, also duplicated the test on the App.test.tsx file. Reinstalled dependencies. Added the .dockerignore file which will ignore the node_modules/ directory when doing a build.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
-import App from './App';
-import { it } from 'vitest'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { it } from "vitest";
 
+it("Renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
 
-it('Renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App/>, div);
-    ReactDOM.unmountComponentAtNode(div);
-})
+it("Renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
