93b51cf907afbe1bf15cdfb8a68776dd4a6607c9
Rayni Nunez Espino <timetopwm@gmail.com>
2022-12-29 15:03:27 -0400

    Added the vitest and jsdom packages to the project, also created the vitest.config.ts file. Since we are using vitest instead of webpack, we are going to need to install these dependencies and create the vitest to be able to run tests without issues. Vitest is Jest compatible, so everything we can do in Jest we can also do it with Vitest.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,1 +1,11 @@
-export {};
+import React from 'react'
+import ReactDOM from 'react-dom';
+import App from './App';
+import { it } from 'vitest'
+
+
+it('Renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+})
---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig, configDefaults } from 'vitest/config'
+
+export default defineConfig({
+    define: {
+        "import.meta.vitest": "undefined"
+    },
+  test: {
+    environment: "jsdom",
+    exclude: [
+        ...configDefaults.exclude,
+        "**/__mocks__/**",
+        "**/__tests__/**",
+    ],
+    includeSource: ["src/**/*.{ts,tsx}"]
+  },
+})
